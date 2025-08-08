2ab5f11b12f8d94a286c83588875a723839c2994
yacine arkane eddine <khammemzine@gmail.com>
2025-08-08 21:09:59 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+// استيراد جميع الصفحات من مجلد routes
 import Home from './routes/Home';
-import Quran from './routes/Quran';
 import Azkar from './routes/Azkar';
+import Quran from './routes/Quran';
 import Library from './routes/Library';
 import Activities from './routes/Activities';
 import Experiments from './routes/Experiments';
@@ -10,46 +12,32 @@ import Ocean from './routes/Ocean';
 import Space from './routes/Space';
 import Story from './routes/Story';
 import Parents from './routes/Parents';
-import Contact from './routes/Contact';
 import About from './routes/About';
+import Contact from './routes/Contact';
+import Login from './routes/Login';
+import Register from './routes/Register';
+import Dashboard from './routes/Dashboard';
 
 function App() {
   return (
     <Router>
-      <div className="container">
-        <h1>مرحبًا بكم في فضاء العبقري الصغير!</h1>
-        <nav>
-          <ul>
-            <li><Link to="/">الرئيسية</Link></li>
-            <li><Link to="/quran">قرآني</Link></li>
-            <li><Link to="/azkar">الأذكار</Link></li>
-            <li><Link to="/library">مكتبتي</Link></li>
-            <li><Link to="/activities">الأنشطة</Link></li>
-            <li><Link to="/experiments">التجارب</Link></li>
-            <li><Link to="/ocean">البحار والمحيطات</Link></li>
-            <li><Link to="/space">الفضاء</Link></li>
-            <li><Link to="/story">قصة وعبرة</Link></li>
-            <li><Link to="/parents">ركن الآباء</Link></li>
-            <li><Link to="/contact">اتصل بنا</Link></li>
-            <li><Link to="/about">عن الموقع</Link></li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/quran" element={<Quran />} />
-          <Route path="/azkar" element={<Azkar />} />
-          <Route path="/library" element={<Library />} />
-          <Route path="/activities" element={<Activities />} />
-          <Route path="/experiments" element={<Experiments />} />
-          <Route path="/ocean" element={<Ocean />} />
-          <Route path="/space" element={<Space />} />
-          <Route path="/story" element={<Story />} />
-          <Route path="/parents" element={<Parents />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/azkar" element={<Azkar />} />
+        <Route path="/quran" element={<Quran />} />
+        <Route path="/library" element={<Library />} />
+        <Route path="/activities" element={<Activities />} />
+        <Route path="/experiments" element={<Experiments />} />
+        <Route path="/ocean" element={<Ocean />} />
+        <Route path="/space" element={<Space />} />
+        <Route path="/story" element={<Story />} />
+        <Route path="/parents" element={<Parents />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </Router>
   );
 }
