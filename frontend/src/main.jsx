031f8882af18e8688056360525b7741c2354e387
yacine arkane eddine <khammemzine@gmail.com>
2025-08-08 19:32:11 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,10 @@
-// frontend/src/main.jsx
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // بدل BrowserRouter
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.rtl.min.css';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
